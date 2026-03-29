e4e0025b29b5552c3caaad5949249c0760f93581
i999rri <melody.hina.mk3@gmail.com>
2026-03-29 17:37:37 +0900

    feat: add Windows platform to embedded apprt (GHOSTTY_PLATFORM_WINDOWS, HWND/HDC/HGLRC)

--- include/ghostty.h
+++ include/ghostty.h
@@ -43,6 +43,7 @@ typedef enum {
   GHOSTTY_PLATFORM_INVALID,
   GHOSTTY_PLATFORM_MACOS,
   GHOSTTY_PLATFORM_IOS,
+  GHOSTTY_PLATFORM_WINDOWS,
 } ghostty_platform_e;
 
 typedef enum {
@@ -431,9 +432,16 @@ typedef struct {
   void* uiview;
 } ghostty_platform_ios_s;
 
+typedef struct {
+  void* hwnd;
+  void* hdc;
+  void* hglrc;
+} ghostty_platform_windows_s;
+
 typedef union {
   ghostty_platform_macos_s macos;
   ghostty_platform_ios_s ios;
+  ghostty_platform_windows_s windows;
 } ghostty_platform_u;
 
 typedef enum {
--- src/apprt/embedded.zig
+++ src/apprt/embedded.zig
@@ -343,6 +343,7 @@ pub const App = struct {
 pub const Platform = union(PlatformTag) {
     macos: MacOS,
     ios: IOS,
+    windows: Windows,
 
     // If our build target for libghostty is not darwin then we do
     // not include macos support at all.
@@ -356,6 +357,15 @@ pub const Platform = union(PlatformTag) {
         uiview: objc.Object,
     } else void;
 
+    pub const Windows = if (builtin.target.os.tag == .windows) struct {
+        /// The window handle to render the surface on.
+        hwnd: std.os.windows.HWND,
+        /// The device context for OpenGL rendering.
+        hdc: ?*anyopaque,
+        /// The OpenGL rendering context.
+        hglrc: ?*anyopaque,
+    } else void;
+
     // The C ABI compatible version of this union. The tag is expected
     // to be stored elsewhere.
     pub const C = extern union {
@@ -365,6 +375,12 @@ pub const Platform = union(PlatformTag) {
 
         ios: extern struct {
             uiview: ?*anyopaque,
+        },
+
+        windows: extern struct {
+            hwnd: ?*anyopaque,
+            hdc: ?*anyopaque,
+            hglrc: ?*anyopaque,
         },
     };
 
@@ -385,6 +401,17 @@ pub const Platform = union(PlatformTag) {
                     break :ios error.UIViewMustBeSet);
                 break :ios .{ .ios = .{ .uiview = uiview } };
             } else error.UnsupportedPlatform,
+
+            .windows => if (Windows != void) windows: {
+                const config = c_platform.windows;
+                const hwnd: std.os.windows.HWND = @ptrCast(config.hwnd orelse
+                    break :windows error.HWNDMustBeSet);
+                break :windows .{ .windows = .{
+                    .hwnd = hwnd,
+                    .hdc = config.hdc,
+                    .hglrc = config.hglrc,
+                } };
+            } else error.UnsupportedPlatform,
         };
     }
 };
@@ -395,6 +422,7 @@ pub const PlatformTag = enum(c_int) {
 
     macos = 1,
     ios = 2,
+    windows = 3,
 };
 
 pub const EnvVar = extern struct {
